3bcf7c0e9e3c63ed60bf6abceb9df0432eaf5b7e
AlexandrAkimov <akimovy14072018@yandex.ru>
2021-11-28 20:55:42 +0300

    fix

--- controllers/friendController.js
+++ controllers/friendController.js
@@ -1,7 +1,7 @@
 const { Friends } = require('../models/models')
 const {Sequelize} = require('sequelize')
 
-const pool = new Sequelize('' + process.env.DB_URL, {
+const pool = new Sequelize('postgres://kqpclnyikvhceo:987056e8ad626c4b0c55a991745326d7bcdac45a3fd695a156194e27c4e5c539@ec2-34-250-16-127.eu-west-1.compute.amazonaws.com:5432/d5pa1486hea44g', {
   ssl: true,
   dialect: 'postgres',
   database: process.env.DB_NAME,
--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 const {Sequelize} = require('sequelize')
 
-module.exports = new Sequelize('' + process.env.DB_URL, {
+module.exports = new Sequelize('postgres://kqpclnyikvhceo:987056e8ad626c4b0c55a991745326d7bcdac45a3fd695a156194e27c4e5c539@ec2-34-250-16-127.eu-west-1.compute.amazonaws.com:5432/d5pa1486hea44g', {
   ssl: true,
   dialect: 'postgres',
   database: process.env.DB_NAME,
